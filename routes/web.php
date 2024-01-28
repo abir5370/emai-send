d347ff48845d9577794c8d3c324f99706fe1ffc4
abir5370 <abir537011@gmail.com>
2024-01-29 01:21:17 +0600

    seeder

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [FrontendController::class,'index']);
 Route::post('send', [FrontendController::class,'send'])->name('send.mail');
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
